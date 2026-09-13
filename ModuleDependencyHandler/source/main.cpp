f627f017e1937eb87123a4200e69c4aee1874e4f
CapibaraEngineer <CapibaraEngineer@github.com>
2026-09-13 13:01:11 -0300

    The pointers... They are shared

--- ModuleDependencyHandler/source/main.cpp
+++ ModuleDependencyHandler/source/main.cpp
@@ -1,23 +1,23 @@
+#include <cstddef>
+#include <cstdio>
 #include <expected>
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <memory>
 #include <regex>
 #include <string>
 #include <vector>
 
 namespace fs = std::filesystem;
 
+using moduleRepresentationPosiiton = size_t; 
+
 struct moduleRepresentation {
 	bool isPartition = false;
 	std::string moduleName;
 	fs::path definitionFile{};
-	std::vector<std::string> dependencies;
-};
-
-struct level {
-	std::vector<moduleRepresentation> modules;
-	std::size_t positionInList = 0; 
+	std::vector<std::shared_ptr<moduleRepresentation>> dependencies;
 };
 
 // TODO: make fileExtension a std::span so it can take multiple extensions
@@ -123,7 +123,7 @@ struct level {
 	return importedPartitions;
 }
 
-std::expected<moduleRepresentation, std::string> buildModuleRepresentationFromFile(const fs::path& file) {
+std::expected<moduleRepresentation, std::string> buildHalfModuleRepresentationFromFile(const fs::path& file) {
 	moduleRepresentation builtModule;
 	builtModule.definitionFile = file;
 
@@ -147,23 +147,30 @@ std::expected<moduleRepresentation, std::string> buildModuleRepresentationFromFi
 		builtModule.isPartition = true;
 	}
 
-	builtModule.dependencies.append_range(checkFileForModulesImports(file));
-	builtModule.dependencies.append_range(checkFileForPartitionsImports(file));
-
 	return builtModule;
 }
 
 
-[[nodiscard]] std::vector<moduleRepresentation> findModules(const std::vector<fs::path>& moduleFiles) {
-	std::vector<moduleRepresentation> foundModules; 
+[[nodiscard]] std::vector<std::shared_ptr<moduleRepresentation>> constructHalfModulesReps(const std::vector<fs::path>& moduleFiles) {
+	std::vector<std::shared_ptr<moduleRepresentation>> foundModules; 
 
 	foundModules.reserve(moduleFiles.size()); // This prealocates the exact amount. the only time this over alocates is if there are invalid files.
 	for(const fs::path& file : moduleFiles) {
-		const auto builtModuleResult = buildModuleRepresentationFromFile(file);
+		const auto builtModuleResult = buildHalfModuleRepresentationFromFile(file);
 		if(builtModuleResult) {
-			foundModules.push_back(builtModuleResult.value());
+			foundModules.push_back(std::make_shared<moduleRepresentation>(builtModuleResult.value()));
 		} else {
 			std::cout << builtModuleResult.error() << "\n";
+		}
+	}
+ 
+	for(const std::shared_ptr<moduleRepresentation>& moduleI : foundModules) {
+		for(const std::shared_ptr<moduleRepresentation>& moduleJ: foundModules) {
+			if(moduleI->moduleName == moduleJ->moduleName) {
+				std::cerr << "duplicate module" << moduleI->definitionFile << " and " << moduleJ->definitionFile << " define thse same name!\n";
+				std::cerr << "Aborting, damn you\n";
+				std::abort();
+			}
 		}
 	}
 
@@ -172,11 +179,96 @@ std::expected<moduleRepresentation, std::string> buildModuleRepresentationFromFi
 
 
 
+[[nodiscard]] std::shared_ptr<moduleRepresentation> CompleteModuleRepresentation(const std::shared_ptr<moduleRepresentation>& currentModule, const std::vector<std::shared_ptr<moduleRepresentation>>& halfModulesReps) {
+	std::shared_ptr<moduleRepresentation> fullModule = currentModule;
+	// There is probaly some better way to build dependenciesNames, but I'm to lazy to do it
+	std::vector<std::string> dependenciesNames = checkFileForModulesImports(currentModule->definitionFile);
+	dependenciesNames.append_range(checkFileForPartitionsImports(currentModule->definitionFile));
+
+	for (const auto& dependencyName : dependenciesNames) {
+
+		auto foundDependecy = std::ranges::find_if(
+			halfModulesReps,
+			[&dependencyName](const std::shared_ptr<moduleRepresentation>& orangutango) -> bool {
+				return orangutango->moduleName == dependencyName;
+			}
+		);
+
+		if(foundDependecy == halfModulesReps.end()) {
+			std::cerr << dependencyName << " in " << currentModule->definitionFile << " does NOT exist!\n";
+			std::cerr << "Aborting, damn you\n";
+			std::abort();
+		}
+
+		if(currentModule == *foundDependecy) {
+			std::cerr << currentModule->moduleName << " in " << currentModule->definitionFile << " depends on itself, dumbass\n";
+			std::cerr << "Aborting, damn you\n";
+			std::abort();
+		}
+
+		fullModule->dependencies.push_back(*foundDependecy);
+	}
+
+	return fullModule;	
+}
+
+std::vector<std::shared_ptr<moduleRepresentation>> completeAllModuleRepresentations(const std::vector<std::shared_ptr<moduleRepresentation>>& halfModulesReps) {
+	std::vector<std::shared_ptr<moduleRepresentation>> fullModules;
+	fullModules.reserve(halfModulesReps.size());
+	for(const auto& halfRep : halfModulesReps) {
+		fullModules.push_back(CompleteModuleRepresentation(halfRep, halfModulesReps)); 
+	}
+	return fullModules;
+}
+
+std::vector<std::shared_ptr<moduleRepresentation>> buildAllModuleRepresentations(const std::vector<fs::path>& moduleFiles) {
+	const std::vector<std::shared_ptr<moduleRepresentation>> halfOnes = constructHalfModulesReps(moduleFiles);
+	const std::vector<std::shared_ptr<moduleRepresentation>> completeModules = completeAllModuleRepresentations(halfOnes);
+	return completeModules;
+}
+
+[[deprecated("I probaly should delete this function")]] moduleRepresentationPosiiton getModulePosFromName(const std::string& name, const std::vector<moduleRepresentation>& allModules) {
+	for(moduleRepresentationPosiiton i = 0; i < allModules.size(); i++) {
+		if(allModules.at(i).moduleName == name) {
+			return i;
+		}
+	}
+	static constexpr std::size_t invalid = 8446744073709551615;
+	return invalid;
+}
+
+void buildModuleDepdencyGraph(const std::vector<moduleRepresentation>& allModules) {
+	
+}
+
+/*bool checkModuleDependenciesCorrectnes(const moduleRepresentation& workModule, const std::vector<moduleRepresentation>& allModules) {
+	size_t foundDependencies = 0;
+	for(const std::string& dependecy : workModule.dependencies) {
+		for(const moduleRepresentation& compared : allModules) {
+			if(compared.moduleName == dependecy) {
+				foundDependencies += 1;
+			}
+		}
+	}
+	if (foundDependencies == workModule.dependencies.size()) {
+		return true;
+	}
+	if(foundDependencies < workModule.dependencies.size()) {
+		return false;
+	}
+	if(foundDependencies > workModule.dependencies.size()) {
+		std::cout << "What\n";
+		return false;
+	}
+	std::cout << "Reality is a lie\n";
+	return false;
+}*/
+
 void dunnoModuleDependencyGraph(const fs::path& searchPath) {
 	const std::vector<fs::path> foundModuleFiles = findFilesByExtension(searchPath, ".cppm");
 
-	const std::vector<moduleRepresentation> modulesRepresentations = findModules(foundModuleFiles);
-	
+	const std::vector<std::shared_ptr<moduleRepresentation>> allModules = buildAllModuleRepresentations(foundModuleFiles);
+
 
 	
 }
